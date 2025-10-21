4ca7552f12c70f4b398b10372352b1578f84c1ba
johnsonluu517 <luujohnson07@gmail.com>
2025-10-21 21:46:21 +0000

    finished problem 1 and 2

--- Main.java
+++ Main.java
@@ -4,6 +4,27 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    for (int i =1; i<=25; i +=2)
+    {
+      System.out.print(i + " ");
+    }
+    System.out.print("\n");
+
+    int b = 1;
+      for (int a = 17; a<=73; a++)
+    {
+      if (b == 10)
+      {
+        System.out.print(a + "\n");
+        b = 0;
+         b++;
+      }
+      else 
+      {
+        System.out.print(a + " ");
+         b++;
+      }
+      
+    }
   }
 }
