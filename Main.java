e40a261621143d6ed2d9bd7da3d8e5963a178121
johnsonluu517 <luujohnson07@gmail.com>
2025-10-23 21:46:07 +0000

    finished problem 3+4

--- Main.java
+++ Main.java
@@ -26,5 +26,52 @@ public class Main
       }
       
     }
+    int d= 1;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("\n");
+    System.out.println("Type an integer");
+    int input = scan.nextInt();
+     
+     if (input <= 0 || input >= 50)
+     {
+      System.out.print("Error");
+     }
+     else
+     {
+      for (int c = input; c<=50; c++)
+    {
+      if (d == 5)
+      {
+        System.out.print(c + "\n");
+        d = 0;
+         d++;
+      }
+      else 
+      {
+        System.out.print(c + " ");
+         d++;
+      }
+    }
+      
+    }
+   System.out.print("\n");
+  System.out.println("Type a positive integer");
+  int num = scan.nextInt();
+  if (num < 0 )
+  {
+     System.out.println("Error");
   }
+ else
+ { 
+  for (int c = num; c>=0; c--)
+    {
+      if (c % 3 == 0 )
+      {
+        System.out.println(c + " ");
+      }
+  
+    }
+
+ }
+}
 }
